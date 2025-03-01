bddb538c6c73d5f141b2bdad681f9947328be752
muharromi42 <muharromi42@gmail.com>
2025-03-01 23:08:22 +0700

    templating sidebar and navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,164 @@
+<!--
+
+=========================================================
+* Volt Free - Bootstrap 5 Dashboard
+=========================================================
+
+* Product Page: https://themesberg.com/product/admin-dashboard/volt-bootstrap-5-dashboard
+* Copyright 2021 Themesberg (https://www.themesberg.com)
+* License (https://themesberg.com/licensing)
+
+* Designed and coded by https://themesberg.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. Please contact us to request a removal.
+
+-->
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <!-- Primary Meta Tags -->
+    <title>digitour-oi</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="title" content="digitour-oi">
+    <meta name="author" content="Themesberg">
+    <meta name="description"
+        content="Volt Pro is a Premium Bootstrap 5 Admin Dashboard featuring over 800 components, 10+ plugins and 20 example pages using Vanilla JS.">
+    <meta name="keywords"
+        content="bootstrap 5, bootstrap, bootstrap 5 admin dashboard, bootstrap 5 dashboard, bootstrap 5 charts, bootstrap 5 calendar, bootstrap 5 datepicker, bootstrap 5 tables, bootstrap 5 datatable, vanilla js datatable, themesberg, themesberg dashboard, themesberg admin dashboard" />
+    <link rel="canonical" href="https://themesberg.com/product/admin-dashboard/volt-premium-bootstrap-5-dashboard">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://demo.themesberg.com/volt-pro">
+    <meta property="og:title" content="digitour-oi">
+    <meta property="og:description"
+        content="Volt Pro is a Premium Bootstrap 5 Admin Dashboard featuring over 800 components, 10+ plugins and 20 example pages using Vanilla JS.">
+    <meta property="og:image"
+        content="https://themesberg.s3.us-east-2.amazonaws.com/public/products/volt-pro-bootstrap-5-dashboard/volt-pro-preview.jpg">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://demo.themesberg.com/volt-pro">
+    <meta property="twitter:title" content="digitour-oi">
+    <meta property="twitter:description"
+        content="Volt Pro is a Premium Bootstrap 5 Admin Dashboard featuring over 800 components, 10+ plugins and 20 example pages using Vanilla JS.">
+    <meta property="twitter:image"
+        content="https://themesberg.s3.us-east-2.amazonaws.com/public/products/volt-pro-bootstrap-5-dashboard/volt-pro-preview.jpg">
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="120x120"
+        href="{{ asset('/template/assets/img/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32"
+        href="{{ asset('/template/assets/img/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16"
+        href="{{ asset('/template/assets/img/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('/template/assets/img/favicon/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('/template/assets/img/favicon/safari-pinned-tab.svg') }}" color="#ffffff">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+    <!-- Sweet Alert -->
+    <link type="text/css" href="{{ asset('/template/vendor/sweetalert2/dist/sweetalert2.min.css') }}" rel="stylesheet">
+
+    <!-- Notyf -->
+    <link type="text/css" href="{{ asset('/template/vendor/notyf/notyf.min.css') }}" rel="stylesheet">
+
+    <!-- Volt CSS -->
+    <link type="text/css" href="{{ asset('/template/css/volt.css') }}" rel="stylesheet">
+
+    <!-- NOTICE: You can use the _analytics.html partial to include production code specific code & trackers -->
+
+</head>
+
+<body>
+
+    <!-- NOTICE: You can use the _analytics.html partial to include production code specific code & trackers -->
+
+
+    {{-- sidebar --}}
+    @include('layouts.partials.sidebar')
+
+    <main class="content">
+
+        {{-- navbar --}}
+        @include('layouts.partials.navbar')
+
+        @yield('content')
+
+        <footer class="bg-white rounded shadow p-5 mb-4 mt-4">
+            <div class="row">
+                <div class="col-12 col-md-4 col-xl-6 mb-4 mb-md-0">
+                    <p class="mb-0 text-center text-lg-start">© 2019-<span class="current-year"></span> <a
+                            class="text-primary fw-normal" href="https://themesberg.com" target="_blank">Themesberg</a>
+                    </p>
+                </div>
+                <div class="col-12 col-md-8 col-xl-6 text-center text-lg-start">
+                    <!-- List -->
+                    <ul class="list-inline list-group-flush list-group-borderless text-md-end mb-0">
+                        <li class="list-inline-item px-0 px-sm-2">
+                            <a href="https://themesberg.com/about">About</a>
+                        </li>
+                        <li class="list-inline-item px-0 px-sm-2">
+                            <a href="https://themesberg.com/themes">Themes</a>
+                        </li>
+                        <li class="list-inline-item px-0 px-sm-2">
+                            <a href="https://themesberg.com/blog">Blog</a>
+                        </li>
+                        <li class="list-inline-item px-0 px-sm-2">
+                            <a href="https://themesberg.com/contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    </main>
+
+    <!-- Core -->
+    <script src="{{ asset('/template/vendor/@popperjs/core/dist/umd/popper.min.js') }}"></script>
+    <script src="{{ asset('/template/vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+
+    <!-- Vendor JS -->
+    <script src="{{ asset('/template/vendor/onscreen/dist/on-screen.umd.min.js') }}"></script>
+
+    <!-- Slider -->
+    <script src="{{ asset('/template/vendor/nouislider/distribute/nouislider.min.js') }}"></script>
+
+    <!-- Smooth scroll -->
+    <script src="{{ asset('/template/vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js') }}"></script>
+
+    <!-- Charts -->
+    <script src="{{ asset('/template/vendor/chartist/dist/chartist.min.js') }}"></script>
+    <script src="{{ asset('/template/vendor/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js') }}"></script>
+
+    <!-- Datepicker -->
+    <script src="{{ asset('/template/vendor/vanillajs-datepicker/dist/js/datepicker.min.js') }}"></script>
+
+    <!-- Sweet Alerts 2 -->
+    <script src="{{ asset('/template/vendor/sweetalert2/dist/sweetalert2.all.min.js') }}"></script>
+
+    <!-- Moment JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js"></script>
+
+    <!-- Vanilla JS Datepicker -->
+    <script src="{{ asset('/template/vendor/vanillajs-datepicker/dist/js/datepicker.min.js') }}"></script>
+
+    <!-- Notyf -->
+    <script src="{{ asset('/template/vendor/notyf/notyf.min.js') }}"></script>
+
+    <!-- Simplebar -->
+    <script src="{{ asset('/template/vendor/simplebar/dist/simplebar.min.js') }}"></script>
+
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+
+    <!-- Volt JS -->
+    <script src="{{ asset('/template/assets/js/volt.js') }}"></script>
+
+
+</body>
+
 </html>
